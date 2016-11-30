54665d30b6454ac41e08f66574ca567fc91db754
Nick Bernard <nick@nrbernard.com>
2016-11-30 15:06:26 -0800

    Add header component and update favicon

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () =>
+  <div className="header">
+    <h1 className="display-3">What are we reading?</h1>
+    <p className="lead"><a href="https://nrbernard.com">Nick Bernard</a> and <a href="https://www.linkedin.com/in/rebecca-shively-7babab3b">Becca Shively</a> share the articles they read and find interesting, insightful, or infuriating.</p>
+  </div>
+
+export default Header;
